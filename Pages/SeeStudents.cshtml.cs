6c0c1cca760b345f760294578bcbf915692843bd
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-08 00:53:49 -0400

    add basic pages: create and read

---
+++ Pages/SeeStudents.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentManagementSystem.Pages;
+
+public class SeeStudents : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+    
+    public void OnPost()
+    {
+        
+    }
+}
