61a061a0ca88deaafcd112823a274be477395108
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-15 18:08:50 -0400

    fetch with student service and display students

--- Pages/SeeStudents.cshtml.cs
+++ Pages/SeeStudents.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentManagementSystem.Models;
+using StudentManagementSystem.Services;
 
 namespace StudentManagementSystem.Pages;
 
 public class SeeStudents : PageModel
 {
+    private readonly StudentService _studentService;
+
+    public SeeStudents(StudentService studentService)
+    {
+        _studentService = studentService;
+    }
+    public List<Student> Students { get; set; } = new();
+
     public void OnGet()
     {
-        
+        Students = _studentService.GetAllStudents();
     }
     
     public void OnPost()
