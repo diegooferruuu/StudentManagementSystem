05f34564e8481994d097fb166756db61b5dd4bd2
Mcqueen <150268661+CamiloPadillaVillazon@users.noreply.github.com>
2025-09-15 08:45:23 -0400

    Create report generator creator (#3)
    * Create ReportGeneratorInterface
    
    * Create ReportGeneratorCreator

---
+++ Services/Reports/ReportGeneratorCreator.cs
@@ -0,0 +1,16 @@
+﻿using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Services.Reports
+{
+    public abstract class ReportGeneratorCreator
+    {
+        public abstract IReportGenerator createReportGenerator();
+        public  IReportGenerator generateReport(Student student)
+        {
+            IReportGenerator reportGenerator = createReportGenerator();
+            return reportGenerator.generateReport(student);
+
+        }
+
+    }
+}
