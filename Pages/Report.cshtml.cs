cd797bb504b8e5cf5a44e372265e334a06da9651
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-15 19:01:01 -0400

    link student id to report page

--- Pages/Report.cshtml.cs
+++ Pages/Report.cshtml.cs
@@ -1,11 +1,30 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentManagementSystem.Models;
+using StudentManagementSystem.Repositories;
+using StudentManagementSystem.Services.Reports;
 
 namespace StudentManagementSystem.Pages;
 
 public class Report : PageModel
 {
-    public void OnGet()
+    private readonly IStudentRepository _repo;
+    private readonly IReportGenerator _reportGenerator;
+
+    public string? ReportHtml { get; private set; }
+    public Student? Student { get; private set; }
+
+    public Report(IStudentRepository repo, IReportGenerator reportGenerator)
     {
-        
+        _repo = repo;
+        _reportGenerator = reportGenerator;
+    }
+
+    public void OnGet(int id)
+    {
+        Student = _repo.GetStudent(id);
+        if (Student != null)
+        {
+            ReportHtml = _reportGenerator.generateReport(Student);
+        }
     }
 }
