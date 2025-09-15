d075c64a0888db158b7c7ddaf8ef913f2a37c71f
anthnyOnt <133285052+anthnyOnt@users.noreply.github.com>
2025-09-15 10:37:34 -0400

    Update models (#4)
    * add migrations folder to gitignore
    
    * fix readme file misspelling
    
    * remove migrations folder from gitignore
    
    * upgrade student model
    
    * add migration with new student model table

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,7 +5,8 @@ namespace StudentManagementSystem.Models;
 public class Student
 {
     public int StudentId { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string firstName { get; set; } = string.Empty;
+    public string lastName { get; set; } = string.Empty;
 
     public List<Score> Scores { get; set; } = new();
 }
