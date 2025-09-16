1ac53c78da74127eae49c3e57b6d24fe58d7869e
Mcqueen <camilo.padilla@ucb.edu.bo>
2025-09-15 20:42:12 -0400

    Create PdfReportGeneratorCreator

---
+++ Services/Reports/PdfReportGeneratorGreator.cs
@@ -0,0 +1,21 @@
+﻿using StudentManagementSystem.Services;
+using StudentManagementSystem.Services.Reports;
+
+namespace StudentManagementSystem.Services.Reports
+{
+    public class PdfReportGeneratorGreator : ReportGeneratorCreator
+    {
+        private readonly IScoreCalculator _calculator;
+        public PdfReportGeneratorGreator(IScoreCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+        public override IReportGenerator createReportGenerator()
+        {
+            return new PdfReportGenerator(_calculator);
+        }
+
+    }
+}
+
+
