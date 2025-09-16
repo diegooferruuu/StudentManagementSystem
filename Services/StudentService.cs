9ebad54ed348402a61a76494d985c8d17fae1e6b
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-15 22:51:48 -0400

    change student repository instance name _repository for descriptive name

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -6,32 +6,26 @@ namespace StudentManagementSystem.Services;
 
 public class StudentService
 {
-    private readonly IStudentRepository _repository;
+    private readonly IStudentRepository _studentRepository;
     private readonly IScoreCalculator _calculator;
 
-    public StudentService(IStudentRepository repository, IScoreCalculator calculator)
+    public StudentService(IStudentRepository studentRepository, IScoreCalculator calculator)
     {
-        _repository = repository;
+        _studentRepository = studentRepository;
         _calculator = calculator;
     }
-    public Student GetStudent(int id)
+    public Student? GetStudent(int id)
     {
-        return _repository.GetStudent(id);
+        return _studentRepository.GetStudent(id);
     }
     
     public void AddStudent(Student student)
     {
-        _repository.AddStudent(student);
+        _studentRepository.AddStudent(student);
     }
 
     public List<Student> GetAllStudents()
     {
-        return _repository.GetAllStudents();
-    }
-    
-    public double CalculateAverageScore(int id)
-    {
-        Student student = GetStudent(id);
-        return _calculator.CalculateAverage(student);
+        return _studentRepository.GetAllStudents();
     }
 }
