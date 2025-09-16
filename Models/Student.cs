3ebd98b7a6b41e93985f2e17731c334424854a8f
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-16 00:34:16 -0400

    add data annotations to student model

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,7 +5,12 @@ namespace StudentManagementSystem.Models;
 public class Student
 {
     public int StudentId { get; set; }
+    [Required(ErrorMessage = "First name is required")]
+    [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters")]
     public string firstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last name is required")]
+    [StringLength(50, ErrorMessage = "Last name can't be longer than 50 characters")]
     public string lastName { get; set; } = string.Empty;
 
     public List<Score> Scores { get; set; } = new();
