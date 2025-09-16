bdc31d600c9bbb083e3a385cc8ec70a3e1a83d0b
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-16 13:18:12 -0400

    apply new student model to student display pages

--- Pages/AddStudent.cshtml.cs
+++ Pages/AddStudent.cshtml.cs
@@ -1,31 +1,44 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using StudentManagementSystem.Models;
-using StudentManagementSystem.Services;
-
-namespace StudentManagementSystem.Pages;
-
-public class AddStudent: PageModel
-{
+using Microsoft.AspNetCore.Mvc;  
+using Microsoft.AspNetCore.Mvc.RazorPages;  
+using StudentManagementSystem.Models;  
+using StudentManagementSystem.Services;  
+  
+namespace StudentManagementSystem.Pages;  
+  
+public class AddStudent : PageModel  
+{  
     private readonly StudentService _studentService;
-
-    public AddStudent(StudentService studentService)
-    {
+    private readonly IStudentValidation _studentValidation;
+  
+    public AddStudent(StudentService studentService, IStudentValidation studentValidation)  
+    {        
         _studentService = studentService;
-    }
-    public void OnGet()
-    {
+        _studentValidation = studentValidation;
+    }  
+    [BindProperty]  
+    public Student Student { get; set; } = new Student();  
+  
+    public void OnGet()  
+    {    }  
+    
+    public IActionResult OnPost()  
+    {        
+        if (!ModelState.IsValid)  
+        {
+            return Page();  
+        }
+
+        var errors = _studentValidation.ValidateStudent(Student);
+        if (errors.Any())
+        {
+            foreach (var error in errors)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
+            return Page();
+        }
         
-    }
-
-    public void OnPost()
-    {
-        String lastName = Request.Form["lastName"];
-        String firstName = Request.Form["firstName"];
-
-        Student student = new Student();
-        student.FirstName = firstName;
-        student.LastName = lastName;
-        _studentService.AddStudent(student);
-
+        _studentService.AddStudent(Student);  
+        return RedirectToPage("SeeStudents");  
     }
 }
