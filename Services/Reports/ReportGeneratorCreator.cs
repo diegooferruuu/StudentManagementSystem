dadb4428339189d8d2d39c3a64f84db9539af5e2
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-16 00:04:22 -0400

    implement report model

--- Services/Reports/ReportGeneratorCreator.cs
+++ Services/Reports/ReportGeneratorCreator.cs
@@ -5,7 +5,7 @@ namespace StudentManagementSystem.Services.Reports
     public abstract class ReportGeneratorCreator
     {
         public abstract IReportGenerator createReportGenerator();
-        public  string generateReport(Student student)
+        public  Report generateReport(Student student)
         {
             var reportGenerator = createReportGenerator();
             return reportGenerator.generateReport(student);
