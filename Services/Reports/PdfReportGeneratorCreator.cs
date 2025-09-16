241ab5281e62292f21b4f8c045470d136d4325aa
Mcqueen <150268661+CamiloPadillaVillazon@users.noreply.github.com>
2025-09-15 21:22:33 -0400

    Create pfd report generator creator (#9)
    * Create PdfReportGenerator
    
    * Create HtmlReportCreator
    
    * Create HtmlReportGeneratorCreator
    
    * Create PdfReportGeneratorCreator
    
    * Refactoring PdfReportGeneratorCreator

---
+++ Services/Reports/PdfReportGeneratorCreator.cs
@@ -0,0 +1,21 @@
+﻿using StudentManagementSystem.Services;
+using StudentManagementSystem.Services.Reports;
+
+namespace StudentManagementSystem.Services.Reports
+{
+    public class PdfReportGeneratorCreator : ReportGeneratorCreator
+    {
+        private readonly IScoreCalculator _calculator;
+        public PdfReportGeneratorCreator(IScoreCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+        public override IReportGenerator createReportGenerator()
+        {
+            return new PdfReportGenerator(_calculator);
+        }
+
+    }
+}
+
+
