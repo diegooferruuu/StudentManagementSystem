e24a158491ea1e0a3607b40999571a862183b9a9
anthnyOnt <antonio.ontiveros@ucb.edu.bo>
2025-09-15 22:49:56 -0400

    replace student repository for student service

--- Pages/Report.cshtml.cs
+++ Pages/Report.cshtml.cs
@@ -1,27 +1,27 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StudentManagementSystem.Models;
-using StudentManagementSystem.Repositories;
+using StudentManagementSystem.Services;
 using StudentManagementSystem.Services.Reports;
 
 namespace StudentManagementSystem.Pages;
 
 public class Report : PageModel
 {
-    private readonly IStudentRepository _repo;
+    private readonly StudentService _studentService;
     private readonly ReportGeneratorCreator _reportGeneratorCreator;
 
     public string? ReportHtml { get; private set; }
     public Student? Student { get; private set; }
 
-    public Report(IStudentRepository repo, HtmlReportGeneratorCreator reportGeneratorCreator)
+    public Report(StudentService studentService, HtmlReportGeneratorCreator reportGeneratorCreator)
     {
-        _repo = repo;
+        _studentService = studentService;
         _reportGeneratorCreator = reportGeneratorCreator;
     }
 
     public void OnGet(int id)
     {
-        Student = _repo.GetStudent(id);
+        Student = _studentService.GetStudent(id);
         if (Student != null)
         {
             ReportHtml = _reportGeneratorCreator.generateReport(Student);
